d0ef5d9cfaa743591bb8215e3e1381b9b2e1423f
Joachim Breitner <mail@joachim-breitner.de>
2009-10-14 12:49:14 +0000

    Include other tilt direction in calculation, for gravity strength

--- src/HDAPS.hs
+++ src/HDAPS.hs
@@ -5,14 +5,15 @@ import System.IO
 
 hdapsFile = "/sys/devices/platform/hdaps/position"
 
--- | Return unit vector pointing „down“
+-- | Return unit vector pointing „down“, direction depending on the sideways
+-- tilt, length 0 to 1 depending on the front-back-tilt
 readTilt :: IO (Complex Double)
 readTilt = do
 	ex <- doesFileExist hdapsFile
 	if not ex then return (0 :+ (-1)) else do
 	h <- openFile hdapsFile ReadMode
 	str <- hGetLine h
-	let (x,_) = read str :: (Integer,Integer)
+	let (x,y) = read str :: (Integer,Integer)
 	let angle = (500 - fromIntegral x) / 300 * pi - (pi/2)
-	return (cis angle)
-
+	let strength = (150 - abs (500-fromIntegral y)) / 150
+	return $ cis angle * (strength :+ 0)
